5fa849262b38156cfae6417c8a8140376a23a85e
webthethird <ratzbodell@gmail.com>
2023-09-24 06:11:03 -0400

    Multi-chain

--- packages/nextjs/services/web3/wagmiConnectors.tsx
+++ packages/nextjs/services/web3/wagmiConnectors.tsx
@@ -20,7 +20,7 @@ const { onlyLocalBurnerWallet } = scaffoldConfig;
 
 // We always want to have mainnet enabled (ENS resolution, ETH price, etc). But only once.
 export const enabledChains =
-  configuredNetwork.id === 1 ? [configuredNetwork] : [configuredNetwork, chains.mainnet, chains.base, chains.gnosis];
+  configuredNetwork.id === 1 ? [configuredNetwork] : [chains.celo, chains.base, chains.gnosis];
 
 /**
  * Chains for the app
